53e95b3b21221dcf62b35b9d18dd96df24f2d0bd
CrusherNotDrip <90648119+CrusherNotDrip@users.noreply.github.com>
2025-02-22 17:16:07 -0500

    hxvlc + skippable cutscenes (stol- i mean borrowed from psych 1,0)

--- setup/setup-unix.sh
+++ setup/setup-unix.sh
@@ -5,13 +5,14 @@
 # https://haxe.org/download/version/4.2.5/
 haxelib install lime
 haxelib install openfl
-haxelib install flixel
-haxelib install flixel-addons
-haxelib install flixel-ui
+haxelib install flixel 5.8.0
+haxelib install flixel-addons 3.2.3
+haxelib install flixel-ui 2.6.1
 haxelib install flixel-tools
-haxelib install SScript
-haxelib install hxCodec
-haxelib install tjson
-haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
+# SScript is discontinued and it won't let you build with it so.
+# haxelib install SScript 8.1.6
+haxelib install hxvlc 2.0.1 --skip-dependencies
+haxelib install tjson 1.4.0
+haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc

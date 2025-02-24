63f3dd229c72d783990fe831cb5309af23738722
Til Tjardes <til.tjardes@gmail.com>
2025-02-24 08:43:43 -0700

    vs code debugging, also hxdiscord_rpc set to last version with create (this prolly gonna be the last commit)

--- setup/setup-windows.bat
+++ setup/setup-windows.bat
@@ -15,6 +15,6 @@ haxelib install hxvlc 2.0.1 --skip-dependencies
 haxelib install tjson 1.4.0
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
-haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc cf90f46a3bce81d5f8ca5def338fba82fc4f4ec1
 echo Finished!
 pause
